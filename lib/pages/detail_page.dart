db35ee16a23581bb62370749960972e9fc39b199
mbulut14-del <m.bulut14@hotmail.com>
2026-04-10 23:28:47 +0300

    Update detail_page.dart

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -4,7 +4,6 @@ import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:syncfusion_flutter_charts/charts.dart';
 
 import '../models/candle_data.dart';
 import '../models/coin_radar_data.dart';
@@ -13,6 +12,7 @@ import '../models/entry_timing_result.dart';
 import '../models/pump_analysis.dart';
 import '../models/pump_analysis_result.dart';
 import '../models/short_setup_result.dart';
+import '../widgets/candle_chart_widget.dart';
 import '../widgets/entry_timing_card.dart';
 import '../widgets/price_box.dart';
 import '../widgets/pump_analysis_card.dart';
@@ -48,7 +48,6 @@ class _DetailPageState extends State<DetailPage>
   late AnimationController _spinnerController;
   late final String contractName;
   late CoinRadarData selectedCoin;
-  late TrackballBehavior _trackballBehavior;
 
   List<CandleData> candles = [];
   List<CandleData> visibleCandles = [];
@@ -69,18 +68,6 @@ class _DetailPageState extends State<DetailPage>
       vsync: this,
       duration: const Duration(milliseconds: 1100),
     )..repeat();
-
-    _trackballBehavior = TrackballBehavior(
-      enable: true,
-      activationMode: ActivationMode.singleTap,
-      tooltipDisplayMode: TrackballDisplayMode.groupAllPoints,
-      lineType: TrackballLineType.vertical,
-      tooltipSettings: const InteractiveTooltip(
-        enable: true,
-        color: Colors.black87,
-        textStyle: TextStyle(color: Colors.white, fontSize: 10),
-      ),
-    );
 
     fetchDetail();
 
@@ -663,146 +650,6 @@ class _DetailPageState extends State<DetailPage>
     );
   }
 
-  Widget _buildChartCard() {
-    final List<_ChartCandle> chartData = visibleCandles
-        .map((c) => _ChartCandle(
-              time: DateTime.fromMillisecondsSinceEpoch(
-                c.timestamp * 1000,
-                isUtc: true,
-              ).toLocal(),
-              open: c.open,
-              high: c.high,
-              low: c.low,
-              close: c.close,
-            ))
-        .toList();
-
-    final double? lastPrice =
-        chartData.isNotEmpty ? chartData.last.close : null;
-
-    final List<PlotBand> plotBands = setupResult == null
-        ? []
-        : [
-            PlotBand(
-              isVisible: true,
-              start: setupResult!.entry,
-              end: setupResult!.entry,
-              borderWidth: 1.2,
-              borderColor: Colors.blueAccent.withOpacity(0.95),
-              dashArray: const <double>[6, 4],
-            ),
-            PlotBand(
-              isVisible: true,
-              start: setupResult!.stopLoss,
-              end: setupResult!.stopLoss,
-              borderWidth: 1.2,
-              borderColor: Colors.redAccent.withOpacity(0.95),
-              dashArray: const <double>[6, 4],
-            ),
-            PlotBand(
-              isVisible: true,
-              start: setupResult!.target1,
-              end: setupResult!.target1,
-              borderWidth: 1.2,
-              borderColor: Colors.greenAccent.withOpacity(0.95),
-              dashArray: const <double>[6, 4],
-            ),
-            PlotBand(
-              isVisible: true,
-              start: setupResult!.target2,
-              end: setupResult!.target2,
-              borderWidth: 1.2,
-              borderColor: Colors.greenAccent.withOpacity(0.95),
-              dashArray: const <double>[6, 4],
-            ),
-          ];
-
-    return Container(
-      height: 320,
-      padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
-      decoration: BoxDecoration(
-        color: Colors.black.withOpacity(0.35),
-        borderRadius: BorderRadius.circular(18),
-        border: Border.all(
-          color: Colors.orangeAccent.withOpacity(0.35),
-        ),
-      ),
-      child: Stack(
-        children: [
-          SfCartesianChart(
-            plotAreaBorderWidth: 0,
-            margin: EdgeInsets.zero,
-            backgroundColor: Colors.transparent,
-            trackballBehavior: _trackballBehavior,
-            primaryXAxis: DateTimeAxis(
-              majorGridLines: MajorGridLines(
-                width: 0.6,
-                color: Colors.white.withOpacity(0.06),
-              ),
-              axisLine: const AxisLine(width: 0),
-              labelStyle: TextStyle(
-                color: Colors.white.withOpacity(0.55),
-                fontSize: 10,
-              ),
-              intervalType: DateTimeIntervalType.auto,
-            ),
-            primaryYAxis: NumericAxis(
-              opposedPosition: true,
-              decimalPlaces: 6,
-              majorGridLines: MajorGridLines(
-                width: 0.6,
-                color: Colors.white.withOpacity(0.08),
-              ),
-              axisLine: const AxisLine(width: 0),
-              labelStyle: TextStyle(
-                color: Colors.white.withOpacity(0.65),
-                fontSize: 10,
-              ),
-              numberFormat: null,
-              plotBands: plotBands,
-            ),
-            series: <CartesianSeries<_ChartCandle, DateTime>>[
-              CandleSeries<_ChartCandle, DateTime>(
-                dataSource: chartData,
-                xValueMapper: (_ChartCandle data, _) => data.time,
-                lowValueMapper: (_ChartCandle data, _) => data.low,
-                highValueMapper: (_ChartCandle data, _) => data.high,
-                openValueMapper: (_ChartCandle data, _) => data.open,
-                closeValueMapper: (_ChartCandle data, _) => data.close,
-                bearColor: Colors.redAccent,
-                bullColor: Colors.greenAccent,
-                enableSolidCandles: true,
-                spacing: 0.12,
-              ),
-            ],
-          ),
-          if (lastPrice != null)
-            Positioned(
-              top: 10,
-              right: 10,
-              child: Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                decoration: BoxDecoration(
-                  color: Colors.black.withOpacity(0.82),
-                  borderRadius: BorderRadius.circular(6),
-                  border: Border.all(color: Colors.white24),
-                ),
-                child: Text(
-                  _formatPrice(lastPrice),
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 10,
-                    fontWeight: FontWeight.w800,
-                  ),
-                ),
-              ),
-            ),
-        ],
-      ),
-    );
-  }
-
   Widget _buildCenterState({
     required Widget child,
   }) {
@@ -927,7 +774,10 @@ class _DetailPageState extends State<DetailPage>
                     const SizedBox(height: 12),
                     _buildShortSetupCard(),
                     const SizedBox(height: 12),
-                    _buildChartCard(),
+                    CandleChartWidget(
+                      candles: visibleCandles,
+                      setupResult: setupResult,
+                    ),
                     const SizedBox(height: 18),
                     Row(
                       children: [
@@ -1001,20 +851,4 @@ class _DetailPageState extends State<DetailPage>
       ),
     );
   }
-}
-
-class _ChartCandle {
-  final DateTime time;
-  final double open;
-  final double high;
-  final double low;
-  final double close;
-
-  _ChartCandle({
-    required this.time,
-    required this.open,
-    required this.high,
-    required this.low,
-    required this.close,
-  });
 }
